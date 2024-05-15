0d99b5d3943a49754d681f5c80fb261c16c03c2d
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-05-14 23:43:52 -0300

    Creating Employee

--- src/application/Company.java
+++ src/application/Company.java
@@ -1,5 +1,15 @@
 package application;
 
+import model.entities.Employee;
+
 public class Company {
+
+	public static void main(String[] args) {
+		
+		Employee e1 = new Employee("Jack", 1000.00);
+		
+		System.out.println("Name: "+e1.getName()+" - Payment: "+e1.getPayment());
+
+	}
 
 }
--- src/model/entities/Employee.java
+++ src/model/entities/Employee.java
@@ -2,6 +2,33 @@ package model.entities;
 
 public class Employee {
 	
+	private String name;
 	
+	private double payment;
+	
+	public Employee(String name, double payment){
+		this.name=name;
+		this.payment=payment;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPayment() {
+		return payment;
+	}
+
+	public void setPayment(double payment) {
+		this.payment = payment;
+	}
+	
+	public void calculatePayment(double payment) {
+		this.payment = payment - (payment*0.02);
+	}
 
 }
